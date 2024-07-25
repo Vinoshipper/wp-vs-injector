6b76499f52c855d99b8bc7f543f0b1333a719bca
John *Seg* Seggerson <seg@spacebetweenstudios.com>
2024-07-25 15:36:19 -0700

    Fix text domain errors.

--- includes/class-vs-injector-i18n.php
+++ includes/class-vs-injector-i18n.php
@@ -34,7 +34,7 @@ class Vs_Injector_I18n {
 	public function load_plugin_textdomain() {
 
 		load_plugin_textdomain(
-			'vs-injector',
+			'vinoshipper',
 			false,
 			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
 		);
